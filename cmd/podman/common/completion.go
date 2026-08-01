82bcc73bccb7c5795170f7b67b3409fd5d174949
Anuj Bolewar <anujbolewar@gmail.com>
2026-08-01 10:11:49 +0530

    docs: use 'anonymous' as the canonical image-volume value
    The runtime accepts anonymous, tmpfs and ignore for --image-volume, but
    the man page and the shell completion both listed 'bind'. 'bind' is only
    a deprecated alias that gets translated to 'anonymous' during parsing.
    
    Update the man page and the AutocompleteImageVolume completion to list
    the real value so users see what the runtime actually accepts.
    
    Fixes #27674
    
    Signed-off-by: Anuj Bolewar <anujbolewar@gmail.com>

--- cmd/podman/common/completion.go
+++ cmd/podman/common/completion.go
@@ -1151,9 +1151,9 @@ func AutocompleteCgroupMode(_ *cobra.Command, _ []string, _ string) ([]string, c
 }
 
 // AutocompleteImageVolume - Autocomplete image volume options.
-// -> "bind", "tmpfs", "ignore"
+// -> "anonymous", "tmpfs", "ignore"
 func AutocompleteImageVolume(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
-	imageVolumes := []string{"bind", "tmpfs", "ignore"}
+	imageVolumes := []string{"anonymous", "tmpfs", "ignore"}
 	return imageVolumes, cobra.ShellCompDirectiveNoFileComp
 }
 
